041e988546763d98f61991ffd35105eb60d8e6ef
Hanyeol Ko <hanyul9785@gmail.com>
2025-07-03 12:31:29 +0900

    BE : 반환 타입 json 명시

--- PentaSecurity_BE/src/main/java/com/assignment/pentasecurity_be/domain/post/controller/PostController.java
+++ PentaSecurity_BE/src/main/java/com/assignment/pentasecurity_be/domain/post/controller/PostController.java
@@ -4,7 +4,6 @@ import com.assignment.pentasecurity_be.domain.post.dto.PostListRequestDto;
 import com.assignment.pentasecurity_be.domain.post.dto.PostRequestDto;
 import com.assignment.pentasecurity_be.domain.post.dto.PostResponseDto;
 import com.assignment.pentasecurity_be.domain.post.service.PostService;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -21,19 +20,19 @@ public class PostController {
 
     private final PostService postService;
 
-    @PostMapping
+    @PostMapping(produces = "application/json")
     public ResponseEntity<Void> createPost(@RequestBody @Validated PostRequestDto dto) {
         postService.save(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<PostResponseDto> getPostById(@PathVariable("id") @Validated @Min(1) long id) {
 
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
-    @GetMapping
+    @GetMapping(produces = "application/json")
     public ResponseEntity<?> getPosts(@ModelAttribute @Validated PostListRequestDto request) {
         Pageable pageable = PageRequest.of(request.page(), request.size());
         return ResponseEntity.ok(postService.getPostList(request.type(), pageable));
